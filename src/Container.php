bd3ae290630452aadb56d1dca5bc747892a7aa63
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2026-08-05 15:09:40 +0200

    feat: avoid multiple instances

--- src/Container.php
+++ src/Container.php
@@ -4,15 +4,47 @@ namespace DI;
 
 class Container
 {
+    /**
+     * Registered service definitions
+     *
+     * @var array
+     */
     private array $definitions = [];
 
+    /**
+     * Already instantiated services.
+     * Cached to avoid creating multiple instances of the same service.
+     *
+     * @var array
+     */
+    private array $instantiated = [];
+
+    /**
+     * Registers a service definition
+     *
+     * @param string $identifier
+     * @param callable $factory
+     * @return void
+     */
     public function register(string $identifier, callable $factory): void
     {
         $this->definitions[$identifier] = $factory;
     }
 
+    /**
+     * Returns the service associated with the given identifier
+     *
+     * @param string $identifier
+     * @return mixed
+     */
     public function get(string $identifier): mixed
     {
-        return $this->definitions[$identifier]($this);
+        if (isset($this->instantiated[$identifier])) {
+            return $this->instantiated[$identifier];
+        }
+
+        $instance = $this->definitions[$identifier]($this);
+        $this->instantiated[$identifier] = $instance;
+        return $instance;
     }
 }
